201825218c0e8939351fea27ac4811f336eb13ee
Ritchie Simmons <ritchiels.alt@gmail.com>
2023-07-28 23:46:11 -0400

    streak

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,35 +10,37 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
 
-        session.user.id = sessionUser._id.toString();
+            session.user.id = sessionUser._id.toString();
 
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
-            
-            //check if user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-            //if not, create new user
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
+                
+                //check if user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+                //if not, create new user
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
         }
     }
 })
@@ -48,3 +50,4 @@ export { handler as GET, handler as POST };
 //[...nextauth] folder name considered best practice
 //export { handler as GET, handler as POST }; not typical, but recommended in documentation for next authentication
 //username: profile.name.replace(" ", "").toLowerCase(), ensure there are no spaces and its lower case
+//commit streak
