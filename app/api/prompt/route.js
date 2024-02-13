66b703e7c3765ad37192db253765bc94e9bac3ce
Ritchie Simmons <ritchiels.alt@gmail.com>
2024-02-12 23:21:19 -0500

    content

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -11,7 +11,9 @@ export const GET = async (request) => {
 
         if (search) {
             query.$or = [
-
+                // content
+                // username
+                // tags
             ];
         }
 
