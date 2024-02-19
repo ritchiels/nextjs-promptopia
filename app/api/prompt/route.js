e3ae9a9f9c63bdc799d8c92786765021336804a5
Ritchie Simmons <ritchiels.alt@gmail.com>
2024-02-18 22:46:20 -0500

    fin

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -26,4 +26,4 @@ export const GET = async (request) => {
 }
 
 //await Prompt.find({}).populate('creator'); {} finds ALL posts, and will show creator name
-//search feat done
+//search feat finished
